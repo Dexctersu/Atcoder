7cf9efbc9a9617fe694d536a2f4d9e8ee66cac43
Dexctersu <siuuopmta107defh@gmail.com>
2022-06-05 16:22:06 +0900

    追加

--- 241/abc_b.py
+++ 241/abc_b.py
@@ -1,10 +1,24 @@
 n, m = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int, input().split()))
-flag = True
+eacted = [False for _ in range(n)]
 for i in range(m):
-    if b[i] in a:
-        a.remove(b[i])
-    else:
-        flag = False
-print("Yes") if flag else print("No")
+    # 食べれるものがあるかを判定
+    flag = False
+    for j in range(n):
+        if(a[j] == b[i] and not(eacted[j])):
+            eacted[j] = True
+            flag = True
+            break
+    if(not(flag)):
+        print("No")
+        exit()
+print("Yes")
+
+# flag = True
+# for i in range(m):
+#     if b[i] in a:
+#         a.remove(b[i])
+#     else:
+#         flag = False
+# print("Yes") if flag else print("No")
