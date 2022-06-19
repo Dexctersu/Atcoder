d7bf575580e3dfed0df405b907818145200163ce
Dexctersu <siuuopmta107defh@gmail.com>
2022-06-19 13:47:37 +0900

    初中級者が解くべき過去問精選 100 問

---
+++ パ研杯2019/c.py
@@ -0,0 +1,20 @@
+n,m=map(int,input().split())
+A=[list(map(int,input().split())) for _ in range(n)]
+highScore = []
+#tmpに入る個数
+cnt = 0
+for i in range(n):
+    tmp = []
+    for j in range(m-1):
+        for k in range(j+1,m):
+            tmp.append(max(A[i][j],A[i][k]))
+            cnt+=1
+    highScore.append(tmp)
+cnt = cnt//n
+ans = []
+for i in range(cnt):
+    score = 0
+    for j in range(n):
+        score+=highScore[j][i]
+    ans.append(score)
+print(max(ans))
