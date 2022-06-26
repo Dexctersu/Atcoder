a6cae21773946dbb52ae9bb2735381576e8fbc09
Dexctersu <siuuopmta107defh@gmail.com>
2022-06-26 21:01:05 +0900

    初中級者が解くべき過去問精選 100 問

--- 95/abc_c.py
+++ 95/abc_c.py
@@ -1,22 +1,7 @@
 a,b,c,x,y=map(int,input().split())
-ab=a+b
-d=[10**9]
-if ab < 2*c:
-    print(a*x+b*y)
-else:
-    #abピザをmax(x,y)コ買う
-    #余分に買う
-    ans1=max(x,y)*(2*c)
-    #abピザをmin(x,y)コ買う+不足分を買う
-    if x<y:
-        tmp=x*(2*c)
-        y=y-x
-        rem=y*b
-        d.append(tmp+rem)
-    elif y<x:
-        tmp=y*(2*c)
-        x=x-y
-        rem=x*a
-        d.append(tmp+rem)
-    ans2=min(d)
-    print(min(ans1,ans2))
+ans = []
+for k in range(10**6):
+    #kはabピザを買う枚数
+    tmp = 2*c*k + max(x-k,0)*a+max(y-k,0)*b
+    ans.append(tmp)
+print(min(ans))
