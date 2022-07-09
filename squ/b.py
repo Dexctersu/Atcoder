9dd1989f2a2cd6580ae7b0ab621feda365e1c3f6
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-09 18:44:34 +0900

    初中級者が解くべき過去問精選 100 問

--- squ/b.py
+++ squ/b.py
@@ -8,10 +8,12 @@ for i in range(n):
 
 ans = []
 
-for i in range(100):
-    for j in range(i,101):
+for i in range(n):
+    for j in range(n):
         dis = 0
         for k in range(n):
-            dis += abs(A[k]-i)+abs(A[k]-B[k])+abs(B[k]-j)
+            #A[i]:スタート(s)
+            #B[j]:ゴール(t)
+            dis += abs(A[k]-A[i])+abs(A[k]-B[k])+abs(B[k]-B[j])
         ans.append(dis)
 print(min(ans))
