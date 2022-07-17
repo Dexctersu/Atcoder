07d4f78568b11d104d77ad38e3146811f33630bc
Dexctersu <siuuopmta107defh@gmail.com>
2022-07-17 18:41:06 +0900

    ビット全探索

--- aoj/alds1_5_a.py
+++ aoj/alds1_5_a.py
@@ -1,15 +1,23 @@
 n = int(input())
-A = list(map(int,input().split()))
+A = list(map(int, input().split()))
 q = int(input())
-m = list(map(int,input().split()))
+M = list(map(int, input().split()))
 
-flag = False
-for bit in range(1<<n):
-    total = 0
-    for i in range(n):
-        if bit&(1<<i):
-            total+=A[i]
-    if total == 21:
-        flag = True
+#ビット演算で全探索
+def bit_all_search(m):
+    #mになるか？
+    flag = False
+    for bit in range(1 << n):
+        total = 0
+        for i in range(n):
+            if bit & (1<<i):
+                total += A[i]
+        if total == m:
+            flag = True
+    return flag
 
-print(flag)
+for i in range(q):
+    if bit_all_search(M[i]):
+        print("yes")
+    else:
+        print("no")
